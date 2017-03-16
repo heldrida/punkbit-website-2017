b19acc0606c8f6803f9469f21f1e4567a029b58f
Helder <heldrida@gmail.com>
2017-03-16 16:17:17 +0000

    wip, modified the main container size etc

--- src/js/containers/footer.js
+++ src/js/containers/footer.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer />
+  <footer>
+    <div className='footer-content'>
+      <p>my footer</p>
+    </div>
+  </footer>
 )
 
 export default Footer
