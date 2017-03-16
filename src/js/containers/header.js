f69fd87462ff5b5393a6cd5143046daa02e26f43
Helder <heldrida@gmail.com>
2017-03-16 11:04:22 +0000

    created the navigation bar

--- src/js/containers/header.js
+++ src/js/containers/header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { loadImage } from 'reactatouille'
+import Navbar from './navbar'
 
 class Header extends Component {
   render () {
     return (
       <div className='header'>
         <img className='logo' ref='logo' src={loadImage('punkbit-cropped.png')} alt='' />
+        <Navbar />
       </div>
     )
   }
---
+++ src/js/containers/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => (
+  <nav id='main-nav'>
+    <ul>
+      <li><a href='#projects'>projects</a></li>
+      <li><a href='#blog'>blog</a></li>
+      <li><a href='#contact'>contact</a></li>
+    </ul>
+  </nav>
+)
+
+export default Navbar
